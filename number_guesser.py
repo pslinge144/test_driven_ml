5833993d1311396ad3987b7753eee9fc3878d7bf
phil <pslinge144@gmail.com>
2016-04-13 20:43:26 -0400

    Added two more tests to the number guesser

--- number_guesser.py
+++ number_guesser.py
@@ -1,5 +1,10 @@
 class NumberGuesser:
     """Guesses numbers based on the history of your input."""
-
+    def __init__(self):
+        self._guessed_numbers = None
+    def numbers_were(self, guessed_numbers):
+        self._guessed_number = guessed_numbers[0]
+    def number_was(self, guessed_number):
+        self._guessed_number = guessed_number
     def guess(self):
-        return None
+        return self._guessed_number
--- number_guesser_tests.py
+++ number_guesser_tests.py
@@ -14,3 +14,12 @@ def given_one_datapoint_when_asked_to_guess_test():
 
     assert type(guessed_number) is int, 'the answer should be a number'
     assert guessed_number == previously_chosen_number, 'the answer should be the previously chosen number.'
+
+def given_two_datapoint_when_asked_to_guess_test():
+    number_guesser = NumberGuesser()
+    previously_chosen_numbers = [1,2,5]
+    number_guesser.numbers_were(previously_chosen_numbers)
+
+    guessed_number = number_guesser.guess()
+
+    assert guessed_number in previously_chosen_numbers, 'the guess should be one of the previously chosen numbers.'
